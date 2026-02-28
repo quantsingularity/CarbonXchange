cbae86fe4a93d1e125e7f72cbe9f179bc929359f
Abrar Ahmed <abrarahmedny@gmail.com>
2026-02-28 10:42:42 +0500

    Implement updates for better performance

--- code/blockchain/contracts/CarbonCreditToken.sol
+++ code/blockchain/contracts/CarbonCreditToken.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract CarbonCreditToken is ERC20 {
-    address public owner;
+  address public owner;
 
-    constructor() ERC20('CarbonCredit', 'CCO2') {
-        owner = msg.sender;
-        _mint(msg.sender, 1000000 * 10 ** decimals());
-    }
+  constructor() ERC20('CarbonCredit', 'CCO2') {
+    owner = msg.sender;
+    _mint(msg.sender, 1000000 * 10 ** decimals());
+  }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, 'Not authorized');
-        _;
-    }
+  modifier onlyOwner() {
+    require(msg.sender == owner, 'Not authorized');
+    _;
+  }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
+  function mint(address to, uint256 amount) public onlyOwner {
+    _mint(to, amount);
+  }
 }
--- code/blockchain/contracts/Marketplace.sol
+++ code/blockchain/contracts/Marketplace.sol
@@ -2,41 +2,37 @@
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
 contract Marketplace {
-    struct Listing {
-        address seller;
-        uint256 amount;
-        uint256 pricePerToken;
-    }
+  struct Listing {
+    address seller;
+    uint256 amount;
+    uint256 pricePerToken;
+  }
 
-    IERC20 public token;
-    Listing[] public listings;
+  IERC20 public token;
+  Listing[] public listings;
 
-    event NewListing(uint256 listingId, address seller, uint256 amount);
-    event Purchase(address buyer, uint256 listingId, uint256 amount);
+  event NewListing(uint256 listingId, address seller, uint256 amount);
+  event Purchase(address buyer, uint256 listingId, uint256 amount);
 
-    constructor(address _tokenAddress) {
-        token = IERC20(_tokenAddress);
-    }
+  constructor(address _tokenAddress) {
+    token = IERC20(_tokenAddress);
+  }
 
-    function createListing(uint256 amount, uint256 price) external {
-        require(token.transferFrom(msg.sender, address(this), amount), 'Transfer failed');
-        listings.push(Listing(msg.sender, amount, price));
-        emit NewListing(listings.length - 1, msg.sender, amount);
-    }
+  function createListing(uint256 amount, uint256 price) external {
+    require(token.transferFrom(msg.sender, address(this), amount), 'Transfer failed');
+    listings.push(Listing(msg.sender, amount, price));
+    emit NewListing(listings.length - 1, msg.sender, amount);
+  }
 
-    function buyCredits(uint256 listingId, uint256 amount) external payable {
-        Listing storage listing = listings[listingId];
-        require(msg.value == amount * listing.pricePerToken, 'Incorrect payment');
-        require(token.transferFrom(address(this), msg.sender, amount), 'Transfer failed');
-        payable(listing.seller).transfer(msg.value);
-        emit Purchase(msg.sender, listingId, amount);
-    }
+  function buyCredits(uint256 listingId, uint256 amount) external payable {
+    Listing storage listing = listings[listingId];
+    require(msg.value == amount * listing.pricePerToken, 'Incorrect payment');
+    require(token.transferFrom(address(this), msg.sender, amount), 'Transfer failed');
+    payable(listing.seller).transfer(msg.value);
+    emit Purchase(msg.sender, listingId, amount);
+  }
 }
